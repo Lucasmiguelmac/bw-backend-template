7cdc221941a5319c877ee6beb9f08159c446c700
Lucas <lucasmiguelmac@gmail.com>
2022-01-25 18:42:07 +0000

    Safe urls in prod

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+if settings.DEBUG:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+    ]
+else:
+    urlpatterns = [
+        # Safe urls grabbed from .env
+    ]
